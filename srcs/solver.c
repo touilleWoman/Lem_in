d311185d36fb5a8831119c30ae0ad8ae520f5517
Bainhail <ali.nabih@gmail.com>
2019-11-25 21:27:56 +0100

    Algo test + reflexion

--- srcs/solver.c
+++ srcs/solver.c
@@ -6,7 +6,7 @@
 /*   By: nabih <naali@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/25 13:50:24 by nabih             #+#    #+#             */
-/*   Updated: 2019/11/25 14:26:32 by nabih            ###   ########.fr       */
+/*   Updated: 2019/11/25 21:26:47 by nabih            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,23 @@ static void				init_flow(t_lemin *lem, t_flow *f)
 	f->max = (f->start <= f->end) ? f->start : f->end;
 }
 
+uint8_t					check_links(t_lemin *lem, uint32_t indice, char *name)
+{
+	t_path			*tmp;
+
+	tmp = lem->pth[indice];
+	while (tmp != NULL)
+	{
+		lem->dash = tmp->id;
+		if (ft_strcmp(tmp->name[0], name))
+			return (LM_TRUE);
+		else if (ft_strcmp(tmp->name[1], name))
+			return (LM_TRUE);
+		tmp = tmp->next;
+	}
+	return (LM_FALSE);
+}
+
 int8_t					solver(t_lemin *lem)
 {
 	t_flow			f;
@@ -31,3 +48,16 @@ int8_t					solver(t_lemin *lem)
 	init_flow(lem, &f);
 	return (LM_SUCCESS);
 }
+
+/*
+  	   START
+  	 ___/|
+   /	 |
+  2		 1 ---- 5
+  |		 |      |
+  |		 |	    |
+  4 ---- 3		6
+         |	___/
+   		 |/
+		END
+*/

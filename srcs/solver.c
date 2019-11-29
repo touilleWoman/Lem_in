711d6933f8758e161e267f76543b7c44928b69ec
touilleWoman <jie.zhou.ac@gmail.com>
2019-11-29 23:48:19 +0100

    BFS done

--- srcs/solver.c
+++ srcs/solver.c
@@ -12,18 +12,6 @@
 
 #include "solver.h"
 
-t_node		*pop_top_elem(t_list **alst)
-{
-	t_node	*ret;
-
-	ret = NULL;
-	if (alst != NULL && *alst != NULL)
-	{
-		ret = (t_node*)((*alst)->content);
-		*alst = (*alst)->next;
-	}
-	return (ret);
-}
 
 uint8_t		not_visited(t_list *visited, t_path *p)
 {
@@ -36,37 +24,44 @@ uint8_t		not_visited(t_list *visited, t_path *p)
 	return (LM_TRUE);
 }
 
+t_list		*node_list_new(t_node **new)
+{
+	t_list *lst;
+
+	lst = ft_lstnew((void const *)new, sizeof(t_node*));
+	return (lst);
+}
+
 void		iter_paths(t_path *p, t_list **visited, t_list **open, t_lemin *lem)
 {
 	t_node		*adjacen_node;
 
-	printf("0\n");
-
 	while (p)
 	{
 		if (p->flow > 0 && not_visited(*visited, p))
-		// if (p->flow > 0 && not_visited(*visited, p))
 		{
-			printf("flow :%d\n", p->flow);
 			adjacen_node = get_node_in_hash(lem, p->name);
-			printf("2\n");
-
-			ft_lstadd_bot(visited, ft_lstnew((void*)adjacen_node, sizeof(t_node*)));
-			printf("3\n");
-
-			ft_lstadd_bot(open, ft_lstnew((void*)adjacen_node, sizeof(t_node*)));
+			ft_lstadd_bot(visited, node_list_new(&adjacen_node));
+			ft_lstadd_bot(open, node_list_new(&adjacen_node));
 		}
 		p = p->next;
 	}
 }
 
-t_list		*node_list_new(t_node const *new)
-{
-	t_list *lst;
 
-	lst = ft_lstnew((void const *)new, sizeof(t_node));
-	return (lst);
+t_node		**pop_top_elem(t_list **alst)
+{
+	t_node	**ret;
+
+	ret = NULL;
+	if (alst != NULL && *alst != NULL)
+	{
+		ret = (t_node**)((*alst)->content);
+		*alst = (*alst)->next;
+	}
+	return (ret);
 }
+
 
 uint8_t		breadth_first_search(t_lemin *lem)
 {
@@ -74,38 +69,29 @@ uint8_t		breadth_first_search(t_lemin *lem)
 	t_list	*visited;
 	t_node	*start;
 	t_node	*end;
-	t_node	*current;
+	t_node	**current;
 
 	open = NULL;
 	visited = NULL;
 	current = NULL;
 	start = get_node_in_hash(lem, lem->start);
-	printf("|unid[%d] ==> %s (nb_paths:%d)|\n", start->unid, start->name,
-			 start->nb_paths);
 	end = get_node_in_hash(lem, lem->end);
-	open = ft_lstnew((void const *)(&start), sizeof(t_node*));
-	// ft_lstadd_bot(&open, ft_lstnew(start, sizeof(t_node*)));
-	t_node *tmp;
-	tmp =(t_node*)(open->content);
-	printf("==================%s\n", (((t_node*)(open->content))->name));
-	printf("|unid[%d] ==> %s (nb_paths:%d)|\n", tmp->unid, tmp->name,
-			 tmp->nb_paths);
-
-	// ft_lstadd_bot(&visited, ft_lstnew((void*)start, sizeof(t_node*)));
-
-	// while (open != NULL)
-	// {
-	// 	printf("enter!!!!\n");
-	// 	current = pop_top_elem(&open);
-	// 	printf("current : unid[%d] ==> %s (nb_paths:%d)|\n", current->unid, current->name,
-	// 			 current->nb_paths);
-	// 	ft_lstadd_bot(&visited, ft_lstnew((void*)current, sizeof(t_node*)));
-	// 	if (current == end)
-	// 		return (LM_TRUE);
-	// 	print_pth(&(current->path_lst));
-	// 	// iter_paths(current->path_lst, &visited, &open, lem);
-	// 	//free current
-	// }
+	open = node_list_new(&start);
+	// printf("|unid[%d] ==> %s (nb_paths:%d)|\n", (*tmp)->unid, (*tmp)->name,
+	// 		 (*tmp)->nb_paths);
+	visited = node_list_new(&start);
+	while (open != NULL)
+	{
+		current = pop_top_elem(&open);
+		printf("current : unid[%d] ==> %s (nb_paths:%d)|\n", (*current)->unid, (*current)->name,
+				 (*current)->nb_paths);
+		ft_lstadd_bot(&visited, node_list_new(current));
+		if (*current == end)
+			return (LM_TRUE);
+		// print_pth(&(current->path_lst));
+		iter_paths((*current)->path_lst, &visited, &open, lem);
+		//free current
+	}
 	return (LM_FALSE);
 }
 
